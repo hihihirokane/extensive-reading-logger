ee17d9ccc33f1914a7845daab48512cb01f0bdbc
Yᴀᴍᴀᴅᴀ Tanryu <oukitama@gmail.com>
2018-12-24 03:32:26 +0900

    first commit for install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+blackcat="Black Cat - Cideb - Black Cat.tsv"
+cambridge="Cambridge U. Press - Cambridge U..tsv"
+cengage="Cengage_Heinle - Cengage.tsv"
+macmillan="Macmillan ELT - Macmillan.tsv"
+oxford="Oxford U. Press - Oxford U..tsv"
+pearson="Pearson ELT - Penguin.tsv"
+penguin="Penguin ELT - Penguin.tsv"
+
+for i in blackcat cambridge cengage macmillan oxford pearson penguin
+do ln -s db/"${!i}" $i
+done || exit 1
