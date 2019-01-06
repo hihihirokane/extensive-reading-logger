a18218a84768b592ba08d3db534ad47ef4aa1142
Yᴀᴍᴀᴅᴀ Tanryu <oukitama@gmail.com>
2019-01-07 03:41:56 +0900

    made create a map called series-db.map from series to db

--- install.sh
+++ install.sh
@@ -7,7 +7,24 @@ oxford="Oxford U. Press - Oxford U..tsv"
 pearson="Pearson ELT - Penguin.tsv"
 penguin="Penguin ELT - Penguin.tsv"
 
+DBLIST="dblist"
+SERIESDB="series-db.map"
+
+if [ -f "$DBLIST" ]; then
+    cat /dev/null > "$DBLIST"
+fi
+
+if [ -f "$SERIESDB" ]; then
+    cat /dev/null > "$SERIESDB"
+fi
+
 for i in blackcat cambridge cengage macmillan oxford pearson penguin
-do ln -s db/"${!i}" $i
-echo "$i" >> dblist
+do
+   if [ ! -L "$i" ]; then ln -s db/"${!i}" $i; fi
+   if [ -L "$i" ]; then echo "$i" >> "$DBLIST"; fi
 done || exit 1
+
+# ./series.sh
+while read db; do
+    awk 'BEGIN{OFS="\t"}NR>3{print$1,"'$db'"}' $db | sort | uniq >> "$SERIESDB"
+done < "$DBLIST"
